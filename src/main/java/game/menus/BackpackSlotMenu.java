c5815091e68d91dce1f1f795a9c940210c26189e
Kyprian Knopp <kyprian.knopp@icloud.com>
2025-12-29 23:36:53 -0700

    changed control flow to adjust for different actions on the backpack

--- src/main/java/game/menus/BackpackSlotMenu.java
+++ src/main/java/game/menus/BackpackSlotMenu.java
@@ -7,13 +7,19 @@ public class BackpackSlotMenu implements Menu {
     private final Player user;
     private final Item[] backpack;
     private final Inventory inventory;
-    private final boolean isSelling;
+    private final String action;
+    private int backpackSlot;
 
-    public BackpackSlotMenu(Player user, boolean isSelling) {
+    public BackpackSlotMenu(Player user, String action) {
         this.user = user;
         this.inventory = user.getInventory();
         this.backpack = inventory.getBackpack();
-        this.isSelling = isSelling;
+        this.action = action;
+    }
+
+    public BackpackSlotMenu(Player user, String action, int backpackSlot) {
+        this(user, action);
+        this.backpackSlot = backpackSlot;
     }
 
     @Override
@@ -34,7 +40,6 @@ public class BackpackSlotMenu implements Menu {
 
         String pattern = "[1-" + backpack.length + "]";
         if (!input.matches(pattern)) {
-            System.out.println("Error - Please try again.");
             return this;
         }
 
@@ -45,6 +50,24 @@ public class BackpackSlotMenu implements Menu {
             return this;
         }
         
-        return new ConfirmationMenu("Sell/Drop " + item.toString() + "?", new SellDropMenu(user, slot, isSelling, new InventoryMenu(user)), this);
+        switch (action) {
+            case "sell" -> {
+                return new ConfirmationMenu("Sell " + item.toString() + "?", new ActionMenu(user, slot, action, new InventoryMenu(user)), this);
+            }
+            case "drop" -> {
+                return new ConfirmationMenu("Drop " + item.toString() + "?", new ActionMenu(user, slot, action, new InventoryMenu(user)), this);
+
+            }
+            case "combineFirst" -> {
+                return new BackpackSlotMenu(user, "combineSecond", slot);
+            }
+            case "combineSecond" -> {
+                return new ConfirmationMenu("Are you sure you want to combine:\n" + item.toString() + "\n" + backpack[backpackSlot].toString(), new ActionMenu(user, slot, "combine", new InventoryMenu(user)), this);
+            }
+            default -> {
+                System.out.println("Error - Please try again.");
+                return this;
+            }
+        }
     }
 }
