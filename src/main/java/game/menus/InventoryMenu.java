ad4fef00a94a241c8663954f901d5c2dfe05a989
Kyprian Knopp <kyprian.knopp@icloud.com>
2025-12-29 23:38:30 -0700

    added previous menu to go back to after

--- src/main/java/game/menus/InventoryMenu.java
+++ src/main/java/game/menus/InventoryMenu.java
@@ -3,9 +3,16 @@ import game.entities.player.*;
 
 public class InventoryMenu implements Menu {
     private final Player user;
+    private final Menu prevMenu;
 
     public InventoryMenu(Player user) {
         this.user = user;
+        this.prevMenu = new MainMenu(user);
+    }
+
+    public InventoryMenu(Player user, Menu prevMenu) {
+        this.user = user;
+        this.prevMenu = prevMenu;
     }
 
     @Override
@@ -13,6 +20,7 @@ public class InventoryMenu implements Menu {
         System.out.println();
         System.out.println("=== Inventory ===");
         System.out.println(user.getInventory().toString());
+        System.out.println("=================");
         System.out.println("What would you like to do?");
         System.out.println("Equip");
         System.out.println("Sell");
@@ -32,17 +40,17 @@ public class InventoryMenu implements Menu {
                     System.out.println("No items in backpack.");
                     return this;
                 }
-                return new BackpackSlotMenu(user, true);
+                return new BackpackSlotMenu(user, "sell");
             }
             case "drop" -> {
                 if (user.getInventory().isBackpackEmpty()) {
                     System.out.println("No items in backpack.");
                     return this;
                 }
-                return new BackpackSlotMenu(user, false);
+                return new BackpackSlotMenu(user, "drop");
             }
             case "back" -> {
-                return new MainMenu(user);
+                return prevMenu;
             }
             default -> {
                 System.out.println("Error - Please try again");
