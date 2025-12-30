6cd3c66f62da903348decaeccc7b158e1f2f7864
Kyprian Knopp <kyprian.knopp@icloud.com>
2025-12-29 23:35:23 -0700

    Renamed menu, added versatility to the action

--- src/main/java/game/menus/ActionMenu.java
+++ src/main/java/game/menus/ActionMenu.java
@@ -4,29 +4,35 @@ import game.entities.player.Player;
 import game.items.Item;
 import game.items.currencies.Gold;
 
-public class SellDropMenu implements Menu {
+public class ActionMenu implements Menu {
     private final Player user;
     private final int slot;
     private final Item[] backpack;
-    private final boolean isSelling;
+    private final String action;
     private final Menu nextMenu;
 
-    public SellDropMenu(Player user, int slot, boolean isSelling, Menu nextMenu) {
+    public ActionMenu(Player user, int slot, String action, Menu nextMenu) {
         this.user = user;
         this.slot = slot;
         this.backpack = user.getInventory().getBackpack();
-        this.isSelling = isSelling;
+        this.action = action;
         this.nextMenu = nextMenu;
     }
 
     @Override
     public void display() {
-        if (isSelling) {
-            int goldValue = backpack[slot].getGoldValue();
-            user.getInventory().addCurrency(new Gold(0), goldValue);
-            backpack[slot] = null;
-        } else {
-            backpack[slot] = null;
+        switch (action) {
+            case "sell" -> {
+                int goldValue = backpack[slot].getGoldValue();
+                user.getInventory().addCurrency(new Gold(0), goldValue);
+                backpack[slot] = null;
+            }
+            case "drop" -> {
+                backpack[slot] = null;
+            }
+            case "combine" -> {
+
+            }
         }
         System.out.println("=== Success ===");
         System.out.println("Enter any key to continue.");
